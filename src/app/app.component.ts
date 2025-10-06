30e3d19553591fc127ab781faf46f58a165ebc95
rosetmagalhaes <rose_tmagalhaes@hotmail.com>
2025-10-06 17:05:28 -0300

    Pattern para digitar apenas número

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,4 +35,40 @@ export class AppComponent {
       }
     });
   }
+
+permitirSomenteNumeros(event: KeyboardEvent) {
+  const tecla = event.key;
+
+  // permite apenas números e teclas de controle
+  if (!/^[0-9]$/.test(tecla) &&
+      tecla !== 'Backspace' &&
+      tecla !== 'Delete' &&
+      tecla !== 'ArrowLeft' &&
+      tecla !== 'ArrowRight' &&
+      tecla !== "ctrl" &&
+      tecla !== 'Tab') {
+    event.preventDefault(); // bloqueia a digitação
+  }
+}
+
+bloquearColar(event: ClipboardEvent) {
+  event.preventDefault(); // bloqueia colar
+}
+
+bloquearColarMouse(event: Event) {
+  event.preventDefault(); // bloqueia colar via teclado, mouse, drag&drop
+}
+
+bloquearCliqueDireito(event: MouseEvent) {
+  event.preventDefault(); // desabilita menu de contexto (botão direito)
+}
+
+//   permitirSomenteNumeros(event: KeyboardEvent) {
+//   const charCode = event.key.charCodeAt(0);
+//   // permite apenas dígitos de 0 a 9
+//   if (charCode < 48 || charCode > 57) {
+//     event.preventDefault();
+//   }
+// }
+
 }
