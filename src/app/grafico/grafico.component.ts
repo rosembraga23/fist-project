7078dbb196dec0ee48c8e030231148ee9d937372
rosetmagalhaes <rose_tmagalhaes@hotmail.com>
2026-03-10 14:13:14 -0300

    Configuração gráfico

--- src/app/grafico/grafico.component.ts
+++ src/app/grafico/grafico.component.ts
@@ -1,6 +1,7 @@
+import { TextoComponent } from './../texto/texto.component';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import Chart from 'chart.js/auto';
-
+import { Colors, Legend } from 'chart.js';
 @Component({
   selector: 'app-grafico',
   templateUrl: './grafico.component.html',
@@ -8,22 +9,46 @@ import Chart from 'chart.js/auto';
 })
 export class GraficoComponent implements OnInit{
 
+  
+
   @ViewChild("meuCanvas", {static: true}) elemento!: ElementRef
   ngOnInit(){
     new Chart(this.elemento.nativeElement, {
       type: 'bar',
       data: {
-        labels:["Janeiro", "Fevereiro", "Março", "Abril"],
+        labels:["Janeiro", "Fevereiro", "Março", "Abril", "Maio"],
         datasets: [
           {
-              data:[30, 20, 10, 13]
-          }
+              data:[30, 20, 10, 13, 5],
+              backgroundColor: ["blue", "yellow", "pink", "red"],
+              label: "Meses"
+          },
           
         ]
+      },
+      options: {
+        plugins: {
+          title: {
+                display: true,
+                text: 'Título do Gráfico'
+            },
+          subtitle: {
+                display: true,
+                text: 'Custom Chart Subtitle'
+            },
+            legend: {
+              display: true, 
+              
+                labels: {      
+                    // This more specific font property overrides the global property
+                    font: {
+                        size: 24
+                    }
+                }
+            }
+        }
       }
     });
   }
-
-
 
 }
